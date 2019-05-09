b4feefe62a92f6f332a0a24cd9b92596a0d9f98d
dschettler8845 <darien_schettler@hotmail.com>
2019-05-09 11:41:36 -0400

    Removed Password Information From Config File

--- config.py
+++ config.py
@@ -18,6 +18,6 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 10
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Dribble_12'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
